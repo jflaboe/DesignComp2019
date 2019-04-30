fea89a48dac2a2cee243ff568456198f1f359cff
birajp <bparikh@u.northwestern.edu>
2019-04-30 18:41:06 -0500

    PWM values set manually

--- Dc2019_esp32_pwm/Dc2019_esp32_pwm.ino
+++ Dc2019_esp32_pwm/Dc2019_esp32_pwm.ino
@@ -1,46 +1,111 @@
 const int PWM1pin = 14;//A0;   // GPIO pin 14
+const int PWM2pin = 13;
 const int DIR1pin = 32;   // GPIO pin 32
-const int PWM1channel = 0;
+const int DIR2pin = 27;
+const int PWM1channel = 1;
+const int PWM2channel = 0;
 const int freq = 5000;
 const int resolution = 8;
 const int dutyCycle = 5000;
 
+int PWM_motor1 = 0;
+int PWM_motor2 = 0;
+int motor1_dir = HIGH;
+int motor2_dir = HIGH;
+
+/*
+ * Expects serial to have 8 digits.
+ * First three digits are motor 1's PWM value.
+ * Last three digits are motor 2's PWM value.
+ * sign-#-#-#-sign-#-#-#
+ */
+int readPWMString(int* newPWMs) {
+  String value = "";
+  while(Serial.available()) { // while serial has anything inputted
+    int inChar = Serial.read();
+    if(isDigit(inChar)) {
+      Serial.print(inChar);
+      Serial.println();
+      value += (char)inChar;
+    }
+    if(inChar == '\n') {
+      Serial.print("Number is");
+      Serial.println(value.toInt());
+    }
+  }
+  return value.toInt();
+}
+
 void setup() {
+  Serial.begin(9600);
   ledcSetup(PWM1channel,5000,8); // pwm channel, frequency, resolution in bits
+  ledcSetup(PWM2channel, 5000, 8);
   ledcAttachPin(PWM1pin,PWM1channel); // pin, pwm channel
+  ledcAttachPin(PWM2pin,PWM2channel); // pin, pwm channel
 
   pinMode(DIR1pin,OUTPUT); // set direction pin to output
+  pinMode(DIR2pin,OUTPUT); // set direction pin to output
 
-  ledcWrite(PWM1channel,0); // pwm channel, speed 0-255
-  digitalWrite(DIR1pin, LOW); // set direction to cw/ccw
+  ledcWrite(PWM1channel,PWM_motor1); // pwm channel, speed 0-255
+  ledcWrite(PWM2channel,PWM_motor2); // pwm channel, speed 0-255
+  digitalWrite(DIR1pin, motor1_dir); // set direction to cw/ccw
+  digitalWrite(DIR2pin, motor2_dir); // set direction to cw/ccw
 }
 
 void loop() {
-  ledcWrite(PWM1channel,128); // full speed
-  digitalWrite(DIR1pin, LOW); // cw/ccw
+  ledcWrite(PWM1channel,PWM_motor1);
+  ledcWrite(PWM2channel,PWM_motor2);
+  digitalWrite(DIR1pin, motor1_dir); // cw/ccw
+  digitalWrite(DIR2pin, motor2_dir); // cw/ccw
 
-  delay(3000);
+  int newPWMs[2];
+  if(Serial.available()) {
+    readPWMString(&newPWMs);
 
-  ledcWrite(PWM1channel,64); // full speed
-  digitalWrite(DIR1pin, HIGH); // ccw/cw
+    int new_PWM_motor2 = readPWMString();
+//    if(PWM_motor1 != newPWMs[0]) {
+      Serial.print("pwm 1 val (new, old):");
+      Serial.print(newPWMs[0]);
+      Serial.print(", ");
+      Serial.print(PWM_motor1);
+      Serial.println();
+      if(newPWMs[0] < 0) {
+        motor1_dir = LOW;
+        digitalWrite(DIR1pin, motor1_dir); // cw/ccw
+      } else if (newPWMs[0] >= 0) {
+        digitalWrite(DIR1pin, HIGH); // cw/ccw
+        motor1_dir = HIGH;
+        digitalWrite(DIR1pin, motor1_dir); // cw/ccw
+      }
+  
+      ledcWrite(PWM1channel,new_PWM_motor1);
+      PWM_motor1 = new_PWM_motor1;
+//    }
+    
+//    if(PWM_motor2 != newPWMs[1]) {
+      Serial.print("pwm 2 val (new, old):");
+      Serial.print(newPWMs[1]);
+      Serial.print(", ");
+      Serial.print(PWM_motor2);
+      Serial.println();
+      if(newPWMs[1] < 0) {
+        motor2_dir = LOW;
+        digitalWrite(DIR2pin, motor2_dir); // cw/ccw
+      } else if(newPWMs[1] >= 0) {
+         motor2_dir = HIGH;
+         digitalWrite(DIR2pin, HIGH); // cw/ccw
+      }
+         
+      ledcWrite(PWM2channel,new_PWM_motor2); // full speed
+      PWM_motor2 = new_PWM_motor2;
+//    }
+  }
 
+    Serial.print("pwm 1 val:");
+    Serial.println(PWM_motor1);
+
+    Serial.print("pwm 2 val:");
+    Serial.println(PWM_motor2);
+  
   delay(3000);
 }
-
-//void setup() {
-//  ledcSetup(PWM1channel, freq, resolution);
-//  ledcAttachPin(A0, PWM1channel);
-//  pinMode(DIR1pin, OUTPUT);
-//}
-//
-//void loop() {
-//  digitalWrite(DIR1pin, LOW);
-//  for(int d = 0; d < dutyCycle; ++d) {
-//    ledcWrite(PWM1channel, d);
-//  }
-//
-//  delay(2000);
-//  digitalWrite(DIR1pin, HIGH);
-//
-//  delay(2000);
-//}
--- sonar_testing/sonar_testing.ino
+++ sonar_testing/sonar_testing.ino
@@ -1,9 +1,9 @@
 #include "BluetoothSerial.h"
 
-#define triggerPin 32
-#define echoPin 14
+#define triggerPin 14
+#define echoPin 12
 #define LED1Pin 15
-#define LED2Pin 12
+#define LED2Pin 27
 
 BluetoothSerial SerialBT;
 
@@ -17,9 +17,9 @@ void setup() {
 }
 
 void loop() {
-//  if (Serial.available()) {
-//    SerialBT.write(Serial.read());
-//  }
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
   digitalWrite(triggerPin, HIGH);  // sonar pulse out
   delayMicroseconds(10);           // 10 uS delay 
   digitalWrite(triggerPin, LOW);   // turn off triggerPin
@@ -29,11 +29,11 @@ void loop() {
   float distance = duration/148.0;  // in inches
   Serial.print(distance);
   Serial.println();
-  if(SerialBT.available()) {
-    SerialBT.print(distance);
-    //SerialBT.println(" in");
-    Serial.println("Written to bluetooth");
-  }
+//  if(SerialBT.available()) {
+  SerialBT.print(distance);
+  SerialBT.println(" in");
+//  Serial.println("Written to bluetooth");
+//  }
   if(distance < 4.0) {
     digitalWrite(LED1Pin, HIGH);
     digitalWrite(LED2Pin, LOW);
@@ -45,3 +45,43 @@ void loop() {
 
   delay(200);
 }
+ 
+//int trigPin = 11;    // Trigger
+//int echoPin = 12;    // Echo
+//long duration, cm, inches;
+ 
+//void setup() {
+//  //Serial Port begin
+//  Serial.begin (115200);
+//  //Define inputs and outputs
+//  pinMode(trigPin, OUTPUT);
+//  pinMode(echoPin, INPUT);
+//}
+// 
+//void loop() {
+//  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+//  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+//  digitalWrite(trigPin, LOW);
+//  delayMicroseconds(5);
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);
+// 
+//  // Read the signal from the sensor: a HIGH pulse whose
+//  // duration is the time (in microseconds) from the sending
+//  // of the ping to the reception of its echo off of an object.
+//  pinMode(echoPin, INPUT);
+//  duration = pulseIn(echoPin, HIGH);
+// 
+//  // Convert the time into a distance
+//  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+//  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+//  
+//  Serial.print(inches);
+//  Serial.print("in, ");
+//  Serial.print(cm);
+//  Serial.print("cm");
+//  Serial.println();
+//  
+//  delay(125);
+//}
