5286aa3b14ea84dc803ca57ff669f8c6796d0297
birajp <bparikh@u.northwestern.edu>
2019-05-07 23:24:12 -0500

    servo custom angles

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -1,22 +1,22 @@
 #include <ESP32Servo.h>
 
 int servoPin = 21;
-Servo myServo;
-int angle = 0;
+//Servo myServo;
+//int angle = 0;
 
-//int freq = 50;      //servos operate @ 50Hz so we ned to set the freq to that value
-//int channel = 0;    //which channel do we want to use
-//int resolution = 8; //set out resolution for a resonable value
-//int angle = 21; //this is about the halfway point (90deg)
+int freq = 50;      //servos operate @ 50Hz so we ned to set the freq to that value
+int channel = 0;    //which channel do we want to use
+int resolution = 8; //set out resolution for a resonable value
+int angle = 15; //this is about the halfway point (90deg)
 
 void setup() {
   Serial.begin(115200);
   pinMode(servoPin, OUTPUT);
-  myServo.attach(servoPin);
-  myServo.write(0);
-//  ledcSetup(channel, freq, resolution);
-//  ledcAttachPin(servoPin, channel);
-//  ledcWrite(channel, angle);
+//  myServo.attach(servoPin);
+//  myServo.write(0);
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(servoPin, channel);
+  ledcWrite(channel, angle);
 }
 
 void loop() {
@@ -33,11 +33,11 @@ void loop() {
     }
   }
   Serial.println(angle);
-//  for(int i = 10; i < 180; i += 10) {
-//    myServo.write(i);  
-//    delay(50);
-//  }
+//  myServo.write(angle);
+  ledcWrite(channel, angle);
+  
   delay(50);
+
   myServo.write(angle);
 //  ledcWrite(channel, angle);
 //  delay(50);
