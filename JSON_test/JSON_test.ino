d55946c401cbcb1623d09b596261220d8f94b359
birajp <bparikh@u.northwestern.edu>
2019-05-07 23:44:10 -0500

    init JSON

---
+++ JSON_test/JSON_test.ino
@@ -0,0 +1,18 @@
+#include <ArduinoJson.h>
+
+const capacity = JSON_ARRAY_SIZE(2) + JSON_ARRAY_SIZE(6) + JSON_OBJECT_SIZE(4);
+string json_out = "";
+
+void setup() {
+  
+}
+
+void loop() {
+  StaticJsonDocument<capacity> doc;
+  doc["key1"] = "val1";
+  doc["key2"] = "val2";
+  serializeJson(doc, json_out);
+  SerialBT.println(json_out);
+  json_out = "";
+  delay(500);
+}
